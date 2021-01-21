4f201a9b2de7d5c5082023fef88270111101db37
ordaboy <t.samat01@gmail.com>
2021-01-21 10:25:56 +0600

    first activity created

---
+++ Task2/app/src/main/java/com/example/task2/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isEmpty
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textTotalNumber = findViewById<TextView>(R.id.tvTotalNumber)
+
+        val buttonAddStudent = findViewById<Button>(R.id.btnAddStudent)
+
+        val students = mutableListOf<Student>()
+
+        buttonAddStudent.setOnClickListener {
+            val nameInput = findViewById<TextInputLayout>(R.id.tilName)
+            val name = nameInput.editText?.text.toString()
+
+            val surnameInput = findViewById<TextInputLayout>(R.id.tilSurname)
+            val surname = surnameInput.editText?.text.toString()
+
+            var validInput = name.isNotBlank() && surname.isNotBlank()
+
+            if (!validInput) {
+                Toast.makeText(this, "Empty/Blank Input!!", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            for (student in students) {
+                if (student.name == name || student.surname == surname) {
+                    validInput = false
+                    break
+                }
+            }
+
+            if (!validInput) {
+                Toast.makeText(this, "Student is already added!!", Toast.LENGTH_LONG).show()
+            } else {
+                students.add(Student(name, surname))
+                Toast.makeText(this, "Student added!!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
---
+++ Task2/app/src/main/java/com/example/task2/Student.kt
@@ -0,0 +1,3 @@
+package com.example.task2
+
+data class Student(val name: String, val surname: String)
