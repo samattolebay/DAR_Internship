5483dfe2ad40d5b5fc61784d992f794eff78b093
ordaboy <t.samat01@gmail.com>
2021-01-21 13:11:18 +0600

    added recycler view to second activity

--- Task2/app/src/main/java/com/example/task2/SecondActivity.kt
+++ Task2/app/src/main/java/com/example/task2/SecondActivity.kt
@@ -3,12 +3,19 @@ package com.example.task2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val ttt = intent.getParcelableArrayListExtra<Student>("data")
+        val ttt = intent.getParcelableArrayListExtra<Student>("data")  as ArrayList<Student>
+
+        val rv = findViewById<RecyclerView>(R.id.rvListOfStudents)
+
+        val adapter = StudentAdapter(this, ttt)
+        rv.adapter = adapter
     }
 }
---
+++ Task2/app/src/main/java/com/example/task2/StudentAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.task2
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+
+class StudentAdapter(private val context: Context,
+                     private val dataSet: ArrayList<Student>) :
+    RecyclerView.Adapter<StudentAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textStudentInfo = view.findViewById<TextView>(R.id.tvStudentInfo)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textStudentInfo.text = "${position + 1}. ${dataSet[position].name} ${dataSet[position].surname}"
+    }
+
+    override fun getItemCount() = dataSet.size
+
+
+}
